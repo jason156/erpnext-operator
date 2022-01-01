3ea6b0e2f515b20e9eda43e9243a517db38e786e
John McKenzie <john@mkz.io>
2022-01-01 11:13:53 -0600

    Add version property

--- api/v1alpha1/erpnextserver_types.go
+++ api/v1alpha1/erpnextserver_types.go
@@ -20,22 +20,18 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// Important: Run "make" to regenerate code after modifying this file
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // ERPNextServerSpec defines the desired state of ERPNextServer
 type ERPNextServerSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of ERPNextServer. Edit erpnextserver_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Version is the ERPNext server version to install and manage.
+	Version string `json:"version,omitempty"`
 }
 
 // ERPNextServerStatus defines the observed state of ERPNextServer
 type ERPNextServerStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
 //+kubebuilder:object:root=true
